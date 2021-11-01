39ac40b720cb690c4a466c48e8d6fd922bbdc302
Sachin Naranji <snaranji01@gmail.com>
2021-11-01 15:29:19 +0000

    2nd test for createRef function

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,3 +1,4 @@
+const { createRef } = require("../db/seeds/utils.js");
 
 
 describe('createRef', () => {
@@ -7,4 +8,10 @@ describe('createRef', () => {
       const expected = {};
       expect(actual).toEqual(expected);
     });
+    test('Test 2: When passed an array with a single object element, returns a reference object with key-value pairs specified by refObjKey and refObjValue inputs', () => {
+        const inputArray = [{name: "Steven", age: 32, height: 180, }]
+        const [key1Input, key2Input] = ["name", "age"];
+        const expectedOutput = {"Steven": 32};
+        expect(createRef(inputArray, key1Input, key2Input)).toEqual(expectedOutput)
+      });
 })
