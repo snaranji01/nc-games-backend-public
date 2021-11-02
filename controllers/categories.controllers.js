bbcc281e1a66a935f7e3a65bd8a13a87a1faf7fb
Sachin Naranji <snaranji01@gmail.com>
2021-11-02 12:50:48 +0000

    Wrote selectReviewById model, used in selectReviewById controller to send data as response. Attached getReviewById to /reviews/:review_id route of /api router. Now passes status:200 GET request test

--- controllers/categories.controllers.js
+++ controllers/categories.controllers.js
@@ -1,6 +1,19 @@
-const { selectCategories } = require("../models/categories.models.js")
+const { selectCategories, selectReviewById } = require("../models/categories.models.js")
 
 exports.getCategories = async (req,res) => {
     const categoriesArray = await selectCategories();
-    res.status(200).send({categories: categoriesArray})
+    res.status(200).send({categories: categoriesArray});
 }
+
+exports.getReviewById = async (req,res) => {
+    const {review_id} = req.params;
+    const review = await selectReviewById(review_id);
+    console.log(review)
+    res.status(200).send({review});
+}
+
+/* exports.getReviews = async (req,res) => {
+    const reviews = await selectReviews();
+    
+    res.status(200).send({reviews});
+} */
--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,9 +1,13 @@
-const { getCategories } = require('../controllers/categories.controllers.js');
+const { getCategories, getReviewById } = require('../controllers/categories.controllers.js');
 
 const apiRouter = require('express').Router();
 
 //apiRouter.get('/', getApiInfo);
 
 apiRouter.get('/categories', getCategories);
+
+apiRouter.get('/reviews/:review_id', getReviewById);
+
+//apiRouter.get('/reviews', getReviews);
 
 module.exports = apiRouter;
