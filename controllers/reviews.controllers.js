3c12d49eae8714b8a3232438d7389cf6d4356733
Sachin Naranji <snaranji01@gmail.com>
2021-11-02 22:34:32 +0000

    Added patchReviewById controller, and updateReviewById model (including throwing errors through promise rejections in updateReviewById model). Now passes all 3 tests defined for /api/reviews/:review_id PATCH endpoint

--- controllers/reviews.controllers.js
+++ controllers/reviews.controllers.js
@@ -1,10 +1,23 @@
-const { selectReviewById } = require("../models/reviews.models.js");
+const { selectReviewById, updateReviewById } = require("../models/reviews.models.js");
 
 
 exports.getReviewById = async (req, res, next) => {
     try {
         const { review_id } = req.params;
         const review = await selectReviewById(review_id)
+        res.status(200).send({ review });
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+exports.patchReviewById = async (req, res, next) => {
+    try {
+        const { review_id } = req.params;
+        const { inc_votes } = req.body;
+
+        const review = await updateReviewById(review_id, inc_votes)
         res.status(200).send({ review });
     } catch (error) {
         next(error)
--- models/reviews.models.js
+++ models/reviews.models.js
@@ -29,6 +29,36 @@ exports.selectReviewById = async (review_id) => {
     return { ...reviewObj, comment_count: commentCount }
 }
 
+exports.updateReviewById = async (review_id, inc_votes) => {
+    if ( !(/^[\d]+$/.test(review_id)) ) {
+        
+        return Promise.reject({
+            status: 400,
+            route: '/api/review/:review_id',
+            msg: `400 Error: invalid input_id, ${review_id}, provided`
+        })
+    }
+
+    const { rows: [responseCountObj] } = await db.query(`SELECT review_votes FROM reviews WHERE review_id = $1;`, [review_id]);
+    const newReviewUpvotes = responseCountObj.review_votes + parseInt(inc_votes);
+
+    const { rows: query1Response } = await db.query(`UPDATE reviews SET review_votes = $1 WHERE review_id = $2 RETURNING *;`, [newReviewUpvotes, review_id]);
+    const { rows: query2Response } = await db.query(`SELECT COUNT(*) FROM comments WHERE review_id = $1;`, [review_id]);
+
+    if(query1Response.length === 0) {
+        return Promise.reject({
+            status: 404,
+            route: '/api/review/:review_id',
+            msg: `404 Error, no review found with a review_id of ${review_id}`
+        })
+    }
+
+    const reviewObj = query1Response[0];
+    const commentCount = parseInt( query2Response[0].count )
+
+    return { ...reviewObj, comment_count: commentCount }
+}
+
 /* exports.selectReviews = async () => {
     const { rows } = await db.query(`SELECT * FROM reviews;`);
     return rows
