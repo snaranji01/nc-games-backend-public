881880e1ee3964bd17747319ad87533f12b2dcae
Sachin Naranji <snaranji01@gmail.com>
2021-11-02 22:29:11 +0000

    Updated to pass only errors that haven't been handled (previously some handled errors were wrongly being sent through to the handle500error function due to error in if statements)

--- controllers/errors.controllers.js
+++ controllers/errors.controllers.js
@@ -1,21 +1,21 @@
-exports.handle404Error = (req,res,next) => {
-    res.status(404).send({msg: '404 Error: Route not found'})
+exports.handle404Error = (req, res, next) => {
+    res.status(404).send({ msg: '404 Error: Route not found' })
 }
 
-exports.handleCustomErrors = (err,req,res,next) => {
-    if(err.status === 404 && err.route === '/api/review/:review_id') {
-        res.status(404).send({msg: err.msg})
+exports.handleCustomErrors = (err, req, res, next) => {
+    if (err.status === 404 && err.route === '/api/review/:review_id') {
+        res.status(404).send({ msg: err.msg })
     }
-    else if(err.status === 400 && err.route === '/api/review/:review_id') {
-        res.status(400).send({msg: err.msg})
+    else if (err.status === 400 && err.route === '/api/review/:review_id') {
+        res.status(400).send({ msg: err.msg })
     } else {
         next(err)
     }
 
 }
 
-exports.handlePSQLErrors = (err,req,res,next) => {
-    if(err.code === "22P02") {
+exports.handlePSQLErrors = (err, req, res, next) => {
+    if (err.code === "22P02") {
         const errResponse = {
             msg: "400 Error: Bad Request - has caused a SQL Error",
             err
@@ -26,10 +26,10 @@ exports.handlePSQLErrors = (err,req,res,next) => {
     } else {
         next(err)
     }
-    
+
 }
 
-exports.handle500ServerError = (err,req,res,next) => {
+exports.handle500ServerError = (err, req, res, next) => {
     console.log(err);
-    res.status(500).send({msg: `500 Server Error`, err});
+    res.status(500).send({ msg: `500 Server Error`, err });
 }
