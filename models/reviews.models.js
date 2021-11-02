9f8d99d1cbf4754d4a75c8dfaee3612b8471b130
Sachin Naranji <snaranji01@gmail.com>
2021-11-02 16:07:24 +0000

    Defined custom 404 error within getReviewById function for when no reviews with a provided review_id exist

--- models/reviews.models.js
+++ models/reviews.models.js
@@ -3,10 +3,21 @@ const db = require("../db/connection.js")
 
 
 exports.selectReviewById = async (review_id) => {
-    const { rows: [reviewObj] } = await db.query(`SELECT * FROM reviews WHERE review_id = $1;`, [review_id]);
-    const { rows: [queryResponse] } = await db.query(`SELECT COUNT(*) FROM comments WHERE review_id = $1;`, [review_id]);
+    const { rows: query1Response } = await db.query(`SELECT * FROM reviews WHERE review_id = $1;`, [review_id]);
+    const { rows: query2Response } = await db.query(`SELECT COUNT(*) FROM comments WHERE review_id = $1;`, [review_id]);
+    
+    if(query1Response.length === 0) {
+        return Promise.reject({
+            status: 404,
+            route: '/api/review/:review_id',
+            msg: `404 Error, no review found with a review_id of ${review_id}`
+        })
+    }
 
-    return { ...reviewObj, comment_count: parseInt(queryResponse.count) }
+    const reviewObj = query1Response[0];
+    const commentCount = parseInt( query2Response[0].count )
+
+    return { ...reviewObj, comment_count: commentCount }
 }
 
 /* exports.selectReviews = async () => {
