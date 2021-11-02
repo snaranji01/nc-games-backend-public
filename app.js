47f48365be15b9cc2f59059866b6abceb2263189
Sachin Naranji <snaranji01@gmail.com>
2021-11-02 17:03:05 +0000

    Added error handler function for general PSQL errors

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { handle404Error, handleCustomErrors, handle500ServerError } = require('./controllers/errors.controllers.js');
+const { handle404Error, handleCustomErrors, handle500ServerError, handlePSQLErrors } = require('./controllers/errors.controllers.js');
 const apiRouter = require('./routes/apiRouter.js');
 const app = express();
 
@@ -10,7 +10,7 @@ app.use('/api', apiRouter);
 app.all('*', handle404Error);
 
 app.use(handleCustomErrors)
-
+app.use(handlePSQLErrors)
 app.use(handle500ServerError)
 
 module.exports = app;
--- controllers/errors.controllers.js
+++ controllers/errors.controllers.js
@@ -1,12 +1,27 @@
 exports.handle404Error = (req,res,next) => {
     res.status(404).send({msg: '404 Error: Route not found'})
+    next(err)
 }
 
 exports.handleCustomErrors = (err,req,res,next) => {
     if(err.status === 404 && err.route === '/api/review/:review_id') {
         res.status(404).send({msg: err.msg})
     }
-    next(err)
+    next(err);
+}
+
+exports.handlePSQLErrors = (err,req,res,next) => {
+    console.log(`error code: ${err.code}`)
+    if(err.code === "22P02") {
+        const errResponse = {
+            msg: "400 Error: Bad Request - has caused a SQL Error",
+            err
+        };
+        res.status(400).send(errResponse)
+        next(errResponse)
+
+    }
+    next(err);
 }
 
 exports.handle500ServerError = (err,req,res,next) => {
