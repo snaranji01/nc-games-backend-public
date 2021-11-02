b01722f708629e62f4571c5721d3b11d5281c4d9
Sachin Naranji <snaranji01@gmail.com>
2021-11-02 13:35:04 +0000

    Refactored selectReviewById to use async/await syntax

--- models/reviews.models.js
+++ models/reviews.models.js
@@ -3,16 +3,10 @@ const db = require("../db/connection.js")
 
 
 exports.selectReviewById = async (review_id) => {
-    const reviewObj = await db.query(`SELECT * FROM reviews WHERE review_id = $1;`, [review_id])
-                            .then(({rows}) => {
-                                const reviewObj = rows[0];
-                                return db.query(`SELECT COUNT(*) FROM comments WHERE review_id = $1;`, [review_id])
-                                    .then(({rows}) => {
-                                        const comment_count = parseInt(rows[0].count);
-                                        return {...reviewObj, comment_count }
-                                    })
-                            })
-    return reviewObj
+    const { rows: [reviewObj] } = await db.query(`SELECT * FROM reviews WHERE review_id = $1;`, [review_id]);
+    const { rows: [queryResponse] } = await db.query(`SELECT COUNT(*) FROM comments WHERE review_id = $1;`, [review_id]);
+
+    return { ...reviewObj, comment_count: parseInt(queryResponse.count) }
 }
 
 /* exports.selectReviews = async () => {
