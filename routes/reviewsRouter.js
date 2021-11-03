915dfedb2c8d5661d48f2c9e0320614fbe8ceeef
Sachin Naranji <snaranji01@gmail.com>
2021-11-03 14:33:24 +0000

    Added GET request endpoint to '/' route in reviewsRouter, that uses getReviews controller. Now passes first test where no query parameters are specified.

--- routes/reviewsRouter.js
+++ routes/reviewsRouter.js
@@ -1,6 +1,10 @@
-const { getReviewById, patchReviewById } = require('../controllers/reviews.controllers.js');
+const { getReviewById, patchReviewById, getReviews } = require('../controllers/reviews.controllers.js');
 
 const reviewsRouter = require('express').Router();
+
+reviewsRouter
+    .route('/')
+    .get(getReviews)
 
 reviewsRouter
     .route('/:review_id')
