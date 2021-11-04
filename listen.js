6946412a192c3dd5f743b26a569fc0e0f75e1dfc
Sachin Naranji <snaranji01@gmail.com>
2021-11-04 15:57:49 +0000

    Updated to pull Port number from Heroku if one given in process.env, or assign Port 9090.

--- listen.js
+++ listen.js
@@ -1,5 +1,8 @@
+const { PORT = 9090 } = process.env;
+
 const app = require("./app.js");
 
-app.listen(9090, () => {
-    console.log(`Server is listening for requests on Port 9090...`)
+app.listen(PORT, (err) => {
+    if(err) throw err;
+    console.log(`Server is listening for requests on Port ${PORT}...`);
 })
