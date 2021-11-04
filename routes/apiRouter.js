170d0a149727d37beb369d0e7141e4e6f036a99f
Sachin Naranji <snaranji01@gmail.com>
2021-11-04 21:20:45 +0000

    Added '/comments' route and assigned 'commentsRouter' function to it. Now passes tests for /api/comments/:comment_id DELETE endpoint

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,5 +1,6 @@
 const { getApiDescription } = require('../controllers/api-description.controller.js');
 const categoriesRouter = require('./categoriesRouter.js');
+const commentsRouter = require('./commentsRouter.js');
 const reviewsRouter = require('./reviewsRouter.js');
 
 const apiRouter = require('express').Router();
@@ -9,5 +10,7 @@ apiRouter.get('/', getApiDescription);
 apiRouter.use('/categories', categoriesRouter);
 
 apiRouter.use('/reviews', reviewsRouter);
+
+apiRouter.use('/comments', commentsRouter)
 
 module.exports = apiRouter;
