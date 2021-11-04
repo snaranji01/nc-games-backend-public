e4d2ecb10140d126f23bfae49901f6e3ed08589f
Sachin Naranji <snaranji01@gmail.com>
2021-11-04 10:16:16 +0000

    Updated selectReviews to work for valid category query parameters passed into /api/reviews GET endpoint

--- models/reviews.models.js
+++ models/reviews.models.js
@@ -41,7 +41,7 @@ exports.updateReviewById = async (review_id, inc_votes) => {
     }
 
     const { rows: [responseCountObj] } = await db.query(`SELECT review_votes FROM reviews WHERE review_id = $1;`, [review_id]);
-    
+
     if (responseCountObj === undefined) {
         return Promise.reject({
             status: 404,
@@ -63,15 +63,31 @@ exports.updateReviewById = async (review_id, inc_votes) => {
 
 exports.selectReviews = async (sort_by, order, category) => {
     const columnNames = ["owner", "title", "review_id", "category", "review_img_url", "created_at", "review_votes", "comment_count"];
-    const baseSelectReviewsQuery = `SELECT r.*, COUNT(c.review_id) AS "comment_count"
+    let selectReviewsQuery =       `SELECT r.*, COUNT(c.review_id) AS "comment_count"
                                     FROM comments AS c
                                     RIGHT OUTER JOIN reviews AS r
-                                    ON c.review_id = r.review_id
-                                    GROUP BY r.review_id`
+                                    ON c.review_id = r.review_id`
+    
+
+    const {rows: validCategoriesResponse} = await db.query(`SELECT slug FROM categories;`);
+    const validCategories = validCategoriesResponse.map(responseEl => responseEl.slug);
+
+    if (validCategories.includes(category)) {
+        if(/'{1}/.test(category)) {
+            replacedApostropheCategory = category.replace(/'{1}/g, "''");
+            selectReviewsQuery = `${selectReviewsQuery} WHERE r.category='${replacedApostropheCategory}'`;
+        } else {
+            selectReviewsQuery = `${selectReviewsQuery} WHERE r.category='${category}'`;
+        }
+        
+    } 
+
+
+
     if (!sort_by || sort_by === 'created_at') {
-        selectReviewsQuery = `${baseSelectReviewsQuery} ORDER BY r.created_at`
+        selectReviewsQuery = `${selectReviewsQuery} GROUP BY r.review_id ORDER BY r.created_at`
     } else if (columnNames.includes(sort_by)) {
-        selectReviewsQuery = `${baseSelectReviewsQuery} ORDER BY ${sort_by}`
+        selectReviewsQuery = `${selectReviewsQuery} GROUP BY r.review_id ORDER BY ${sort_by}`
     } else {
         return Promise.reject({
             status: 400,
