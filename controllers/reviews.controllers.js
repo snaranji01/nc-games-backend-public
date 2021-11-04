18608a70cea6b9d6630489a31ef52b12b07363d3
Sachin Naranji <snaranji01@gmail.com>
2021-11-04 12:12:06 +0000

    Added 'getReviewCommentsById' controller and 'selectReviewCommentsById' model functions. Now passes 4 most recently defined tests for /api/reviews/:review_id/comments GET endpoint.

--- controllers/reviews.controllers.js
+++ controllers/reviews.controllers.js
@@ -1,11 +1,16 @@
-const { selectReviewById, updateReviewById, selectReviews } = require("../models/reviews.models.js");
+const { selectReviewById, updateReviewById, selectReviews, selectReviewCommentsById } = require("../models/reviews.models.js");
 
 
 exports.getReviewById = async (req, res, next) => {
     try {
         const { review_id } = req.params;
-        const review = await selectReviewById(review_id)
-        res.status(200).send({ review });
+        try {
+            const review = await selectReviewById(review_id)
+            res.status(200).send({ review });
+        } catch (error) {
+            next(error)
+        }
+
     } catch (error) {
         next(error)
     }
@@ -16,9 +21,13 @@ exports.patchReviewById = async (req, res, next) => {
     try {
         const { review_id } = req.params;
         const { inc_votes } = req.body;
+        try {
+            const review = await updateReviewById(review_id, inc_votes)
+            res.status(200).send({ review });
+        } catch (error) {
+            next(error)
+        }
 
-        const review = await updateReviewById(review_id, inc_votes)
-        res.status(200).send({ review });
     } catch (error) {
         next(error)
     }
@@ -28,11 +37,20 @@ exports.patchReviewById = async (req, res, next) => {
 exports.getReviews = async (req, res, next) => {
     try {
         const { sort_by, order, category } = req.query;
-
         const reviews = await selectReviews(sort_by, order, category);
-
         res.status(200).send({ reviews });
     } catch (error) {
         next(error)
     }
+}
+
+exports.getReviewCommentsById = async (req, res, next) => {
+    try {
+        const { review_id } = req.params;
+        const reviewComments = await selectReviewCommentsById(review_id);
+        res.status(200).send({ reviewComments })
+    } catch (error) {
+        next(error)
+    }
+
 }
