7350bbe33ff8a8d774c8fa92aead20ea2147b355
Sachin Naranji <snaranji01@gmail.com>
2021-11-04 18:57:53 +0000

    Added '/' route, and attached 'getApiDescription' controller function to it

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,15 +1,13 @@
+const { getApiDescription } = require('../controllers/api-description.controller.js');
 const categoriesRouter = require('./categoriesRouter.js');
 const reviewsRouter = require('./reviewsRouter.js');
 
 const apiRouter = require('express').Router();
 
-//apiRouter.get('/', getApiInfo);
+apiRouter.get('/', getApiDescription);
 
 apiRouter.use('/categories', categoriesRouter);
 
 apiRouter.use('/reviews', reviewsRouter);
-
-
-//apiRouter.get('/reviews', getReviews);
 
 module.exports = apiRouter;
