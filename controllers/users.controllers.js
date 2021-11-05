8bfb8dc82cf19cff9b9ea9076f5d00eabf89d5dc
Sachin Naranji <snaranji01@gmail.com>
2021-11-05 13:51:40 +0000

    Added 'getUserByUsername' controller function

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,10 +1,20 @@
-const { selectUsers } = require("../models/users.models.js")
+const { selectUsers, selectUserByUsername } = require("../models/users.models.js")
 
 exports.getUsers = async (req, res, next) => {
     try {
         const users = await selectUsers();
         res.status(200).send({ users });
     } catch (error) {
-        next(error)
+        next(error);
+    }
+}
+
+exports.getUserByUsername = async (req, res, next) => {
+    try {
+        const { username } = req.params;
+        const user = await selectUserByUsername(username);
+        res.status(200).send({ user });
+    } catch (error) {
+        next(error);
     }
 }
