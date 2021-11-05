9d7eded40f6fdf4394ee0b4c293889ef93d8fb3d
Sachin Naranji <snaranji01@gmail.com>
2021-11-05 13:51:19 +0000

    Added 'selectUserByUsername' model function

--- models/users.models.js
+++ models/users.models.js
@@ -4,3 +4,20 @@ exports.selectUsers = async () => {
     const { rows: usersResponse } = await db.query(`SELECT * FROM users;`);
     return usersResponse;
 }
+
+exports.selectUserByUsername = async (username) => {
+    const { rows: usersResponse } = await db.query(`SELECT * FROM users;`);
+    const existingUsernames = usersResponse.map(user => user.username);
+
+    if(!existingUsernames.includes(username)) {
+        return Promise.reject({
+            status: 404,
+            route: '/api/users/:user_id',
+            msg: `404 Error: no user found with the provided username, ${username}`
+        })
+    }
+
+    const { rows: [user] } = await db.query(`SELECT * FROM users WHERE username=$1`, [username]);
+    console.log(user)
+    return user;
+}
