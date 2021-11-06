bd1929916f093818d27d71e9b646ae4088ebea2b
Sachin Naranji <snaranji01@gmail.com>
2021-11-06 21:26:18 +0000

    Added .patch endpoint to '/:comment_id' route, and attached 'patchCommentsByCommentId' controller function. Now passes tests for /api/comments/comment_id PATCH endpoint

--- routes/commentsRouter.js
+++ routes/commentsRouter.js
@@ -1,10 +1,11 @@
-const { deleteCommentByCommentId } = require('../controllers/comments.controllers.js');
+const { deleteCommentByCommentId, patchCommentByCommentId } = require('../controllers/comments.controllers.js');
 
 const commentsRouter = require('express').Router();
 
 commentsRouter
     .route('/:comment_id')
     .delete(deleteCommentByCommentId)
+    .patch(patchCommentByCommentId)
 
 
 module.exports = commentsRouter;
