3c7ae716f5b4c3c51fe86c229b9980b39797a0d8
Sachin Naranji <snaranji01@gmail.com>
2021-11-06 21:24:12 +0000

    Added 'patchCommentByCommentId' controller function

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -1,4 +1,4 @@
-const { removeCommentByCommentId } = require("../models/comments.models.js");
+const { removeCommentByCommentId, updateCommentByCommentId } = require("../models/comments.models.js");
 
 
 exports.deleteCommentByCommentId = async (req, res, next) => {
@@ -6,6 +6,19 @@ exports.deleteCommentByCommentId = async (req, res, next) => {
         const { comment_id } = req.params;
         await removeCommentByCommentId(comment_id);
         res.sendStatus(204);
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.patchCommentByCommentId = async (req, res, next) => {
+    try {
+        const { comment_id } = req.params;
+        const { inc_votes } = req.body;
+
+        const updatedComment = await updateCommentByCommentId(comment_id, inc_votes);
+        return res.status(200).send({ updatedComment })
+
     } catch (error) {
         next(error)
     }
