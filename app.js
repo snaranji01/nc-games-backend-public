8daa618b13c7bf5c22d49ebeb14456ebd91ce2bf
Sachin Naranji <snaranji01@gmail.com>
2021-11-22 16:22:57 +0000

    imported cors, then invoked it as a middleware

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const { handle404Error, handleCustomErrors, handle500ServerError, handlePSQLErrors } = require('./controllers/errors.controllers.js');
 const apiRouter = require('./routes/apiRouter.js');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', apiRouter);
 
